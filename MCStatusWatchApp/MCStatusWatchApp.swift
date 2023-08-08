5a114db7b02987c134f550b166bb24016b8b0604
Tomer Shemesh <tomerjshemesh@gmail.com>
2023-08-07 22:55:39 -0400

    add watch app

---
+++ MCStatusWatchApp/MCStatusWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  MCStatusWatchAppApp.swift
+//  MCStatusWatchApp Watch App
+//
+//  Created by Tomer Shemesh on 8/7/23.
+//
+
+import SwiftUI
+
+@main
+struct MCStatusWatchApp_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
