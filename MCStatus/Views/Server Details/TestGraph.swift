0db88c04227ee05b3a6f0b5ba1b6a70704d55c6c
TopScrech <sergei_saliukov@icloud.com>
2025-02-19 22:35:13 +0100

    -

--- MCStatus/Views/Server Details/TestGraph.swift
+++ MCStatus/Views/Server Details/TestGraph.swift
@@ -2,9 +2,9 @@ import SwiftUI
 import Charts
 
 struct SingleLineLollipop: View {
-    @Binding private var data: [Sale]
+    @Binding private var data: [ServerPing]
     
-    init(_ data: Binding<[Sale]>) {
+    init(_ data: Binding<[ServerPing]>) {
         _data = data
     }
     
@@ -13,13 +13,16 @@ struct SingleLineLollipop: View {
     @State private var lineWidth = 2.0
     @State private var chartColor: Color = .blue
     @State private var showSymbols = true
-    @State private var selectedElement: Sale? = SalesData.last30Days[10]
+    
+    @State private var selectedElement: ServerPing? = nil
+    
     @State private var showLollipop = true
     
     var body: some View {
         List {
             Section {
                 chart
+                    .animation(.default, value: data.count)
             }
             
             Section {
@@ -27,18 +30,31 @@ struct SingleLineLollipop: View {
                     .callout()
                 
                 Toggle("Lollipop", isOn: $showLollipop)
+                
+                Button("Add") {
+                    add()
+                }
+                
+                Button("Clear") {
+                    selectedElement = nil
+                    data.removeAll()
+                }
             }
         }
+    }
+    
+    private func add() {
+        data.append(.init(50, date: data.last!.date.addingTimeInterval(31536000)))
     }
     
     private var chart: some View {
         Chart(data, id: \.date) {
             LineMark (
                 x: .value("Date", $0.date),
-                y: .value("Sales", $0.sales)
+                y: .value("Ping", $0.ping)
             )
             .accessibilityLabel($0.date.formatted(date: .complete, time: .omitted))
-            .accessibilityValue("\($0.sales) sold")
+            .accessibilityValue("\($0.ping) ping")
             .lineStyle(StrokeStyle(lineWidth: lineWidth))
             .foregroundStyle(chartColor.gradient)
             .interpolationMethod(.cardinal)
@@ -97,7 +113,8 @@ struct SingleLineLollipop: View {
                             Text("\(selectedElement.date, format: .dateTime.year().month().day())")
                                 .callout()
                                 .foregroundStyle(.secondary)
-                            Text("\(selectedElement.sales, format: .number)")
+                            
+                            Text("\(selectedElement.ping, format: .number)")
                                 .title2(.bold)
                                 .foregroundColor(.primary)
                         }
@@ -125,7 +142,7 @@ struct SingleLineLollipop: View {
         .frame(height: detailChartHeight)
     }
     
-    private func findElement(location: CGPoint, proxy: ChartProxy, geometry: GeometryProxy) -> Sale? {
+    private func findElement(location: CGPoint, proxy: ChartProxy, geometry: GeometryProxy) -> ServerPing? {
         let relativeXPosition = location.x - geometry[proxy.plotAreaFrame].origin.x
         
         if let date = proxy.value(atX: relativeXPosition) as Date? {
@@ -153,89 +170,17 @@ struct SingleLineLollipop: View {
 
 #Preview {
     @Previewable @State var pings = (0..<20).map {
-        ServerPing(Int.random(in: 10...100), date: Date().addingTimeInterval(Double($0)))
+        ServerPing(Int.random(in: 10...100), date: Date().addingTimeInterval(Double($0 * 31536000)))
     }
     
-    @Previewable @State var data = SalesData.last30Days
-    
-    //    PingGraph($pings)
-    SingleLineLollipop($data)
+    SingleLineLollipop($pings)
 }
 
-enum SalesData {
-    static let last30Days = [
-        (day: date(year: 2022, month: 5, day: 8), sales: 168),
-        (day: date(year: 2022, month: 5, day: 9), sales: 117),
-        (day: date(year: 2022, month: 5, day: 10), sales: 106),
-        (day: date(year: 2022, month: 5, day: 11), sales: 119),
-        (day: date(year: 2022, month: 5, day: 12), sales: 109),
-        (day: date(year: 2022, month: 5, day: 13), sales: 104),
-        (day: date(year: 2022, month: 5, day: 14), sales: 196),
-        (day: date(year: 2022, month: 5, day: 15), sales: 172),
-        (day: date(year: 2022, month: 5, day: 16), sales: 122),
-        (day: date(year: 2022, month: 5, day: 17), sales: 115),
-        (day: date(year: 2022, month: 5, day: 18), sales: 138),
-        (day: date(year: 2022, month: 5, day: 19), sales: 110),
-        (day: date(year: 2022, month: 5, day: 20), sales: 106),
-        (day: date(year: 2022, month: 5, day: 21), sales: 187),
-        (day: date(year: 2022, month: 5, day: 22), sales: 187),
-        (day: date(year: 2022, month: 5, day: 23), sales: 119),
-        (day: date(year: 2022, month: 5, day: 24), sales: 160),
-        (day: date(year: 2022, month: 5, day: 25), sales: 144),
-        (day: date(year: 2022, month: 5, day: 26), sales: 152),
-        (day: date(year: 2022, month: 5, day: 27), sales: 148),
-        (day: date(year: 2022, month: 5, day: 28), sales: 240),
-        (day: date(year: 2022, month: 5, day: 29), sales: 242),
-        (day: date(year: 2022, month: 5, day: 30), sales: 173),
-        (day: date(year: 2022, month: 5, day: 31), sales: 143),
-        (day: date(year: 2022, month: 6, day: 1), sales: 137),
-        (day: date(year: 2022, month: 6, day: 2), sales: 123),
-        (day: date(year: 2022, month: 6, day: 3), sales: 146),
-        (day: date(year: 2022, month: 6, day: 4), sales: 214),
-        (day: date(year: 2022, month: 6, day: 5), sales: 250),
-        (day: date(year: 2022, month: 6, day: 6), sales: 146)
-    ].map { Sale(date: $0.day, sales: $0.sales) }
-    
-    /// Total sales for the last 30 days.
-    static var last30DaysTotal: Int {
-        last30Days.map { $0.sales }.reduce(0, +)
-    }
-    
-    static var last30DaysAverage: Double {
-        Double(last30DaysTotal / last30Days.count)
-    }
-    
-    /// Sales by month for the last 12 months.
-    static let last12Months = [
-        (month: date(year: 2021, month: 7), sales: 3952, dailyAverage: 127, dailyMin: 95, dailyMax: 194),
-        (month: date(year: 2021, month: 8), sales: 4044, dailyAverage: 130, dailyMin: 96, dailyMax: 189),
-        (month: date(year: 2021, month: 9), sales: 3930, dailyAverage: 131, dailyMin: 101, dailyMax: 184),
-        (month: date(year: 2021, month: 10), sales: 4217, dailyAverage: 136, dailyMin: 96, dailyMax: 193),
-        (month: date(year: 2021, month: 11), sales: 4006, dailyAverage: 134, dailyMin: 104, dailyMax: 202),
-        (month: date(year: 2021, month: 12), sales: 3994, dailyAverage: 129, dailyMin: 96, dailyMax: 190),
-        (month: date(year: 2022, month: 1), sales: 4202, dailyAverage: 136, dailyMin: 96, dailyMax: 203),
-        (month: date(year: 2022, month: 2), sales: 3749, dailyAverage: 134, dailyMin: 98, dailyMax: 200),
-        (month: date(year: 2022, month: 3), sales: 4329, dailyAverage: 140, dailyMin: 104, dailyMax: 218),
-        (month: date(year: 2022, month: 4), sales: 4084, dailyAverage: 136, dailyMin: 93, dailyMax: 221),
-        (month: date(year: 2022, month: 5), sales: 4559, dailyAverage: 147, dailyMin: 104, dailyMax: 242),
-        (month: date(year: 2022, month: 6), sales: 1023, dailyAverage: 170, dailyMin: 120, dailyMax: 250)
-    ]
-    
 //    /// Total sales for the last 12 months.
 //    static var last12MonthsTotal: Int {
 //        last12Months.map { $0.sales }.reduce(0, +)
 //    }
-//    
+//
 //    static var last12MonthsDailyAverage: Int {
 //        last12Months.map { $0.dailyAverage }.reduce(0, +) / last12Months.count
 //    }
-}
-
-struct Sale {
-    let date: Date
-    var sales: Int
-}
-
-func date(year: Int, month: Int, day: Int = 1, hour: Int = 0, minutes: Int = 0, seconds: Int = 0) -> Date {
-    Calendar.current.date(from: DateComponents(year: year, month: month, day: day, hour: hour, minute: minutes, second: seconds)) ?? Date()
-}
