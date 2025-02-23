dda88e4b605289b481e98cfd398e5604e386fc81
TopScrech <sergei_saliukov@icloud.com>
2025-02-23 21:34:24 +0100

    CloudKit/CoreData logs disabled

--- MCStatusDataLayer/Sources/MCStatusDataLayer/Networking/Helpers/SwiftDataHelper.swift
+++ MCStatusDataLayer/Sources/MCStatusDataLayer/Networking/Helpers/SwiftDataHelper.swift
@@ -11,9 +11,7 @@ public class SwiftDataHelper {
                     isStoredInMemoryOnly: false,
                     allowsSave: true,
                     groupContainer: ModelConfiguration.GroupContainer.automatic,
-//                    groupContainer: ModelConfiguration.GroupContainer.identifier("group.dev.topscrech.MC-Stats"),
                     cloudKitDatabase: ModelConfiguration.CloudKitDatabase.automatic
-//                    cloudKitDatabase: ModelConfiguration.CloudKitDatabase.private("iCloud.dev.topscrech.MC-Stats")
                 )
                 
                 return try ModelContainer(for: SavedMinecraftServer.self, configurations: config)
